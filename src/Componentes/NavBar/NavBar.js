75b67b5079a443e8415c1fde58aa7159c5fef9c1
henry.monroy <henry_monroy7@hotmail.com>
2023-08-07 21:09:50 -0500

    PreEntrega1 MonroyHenry

---
+++ src/Componentes/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavBar = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>E-Commerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>              
+              <NavDropdown title="Categorias" id="basic-nav-dropdown">
+                <NavDropdown.Item>Categoria 1</NavDropdown.Item>
+                <NavDropdown.Item>
+                  Categoria 2
+                </NavDropdown.Item>
+                <NavDropdown.Item>Categoria 3</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item>
+                  Otras categorias
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default NavBar;
